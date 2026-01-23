a79ab998ed1b5a3c1aedcccfc529c542818ffd7f
anushkaadak2684 <anushkaadakclassx@gmail.com>
2026-01-23 22:15:35 +0530

    Added Advance Patterns

---
+++ Patterns/Butterfly.java
@@ -0,0 +1,22 @@
+public class Butterfly {
+    public static void main(String[] args) {
+        for (int i=1;i<=4;i++){
+            for(int j=1;j<=8;j++){
+                if(j<=i || j>8-i)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for (int i=4;i>=1;i--){
+            for(int j=1;j<=8;j++){
+                if(j<=i || j>8-i)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Patterns/Diamond.java
@@ -0,0 +1,22 @@
+public class Diamond {
+    public static void main(String[] args) {
+        for (int i=1;i<=4;i++){
+            for(int j=1;j<=7;j++){
+                if(j>4-i && j<4+i)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for (int i=4;i>=1;i--){
+            for(int j=1;j<=7;j++){
+                if(j>4-i && j<4+i)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
